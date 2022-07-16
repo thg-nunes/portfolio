9826f64dce9b53414376909299082b51552325de
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-16 14:13:30 -0300

    feature: inicio config styled-components

---
+++ styles/global/global-style.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  /* essa estilização na media-screen diminui a font de 16px(padrão) para 15px e 14px respectivamente */
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    color: ${({theme}) => theme.font.colors.white200};
+    background: ${({theme}) => theme.colors.background.gray700};
+  }
+
+  /* essa estilização é feita pelo fato de cada um desses elementos "não respeitaram" a font definida na aplicação,
+    então "obrigo" cada um a usar a font escolhida
+  */
+  body, input, textarea, select, button {
+    font: 400 1rem 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`
---
+++ styles/global/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  font: {
+    fontFamily: "'Roboto', sans-serif",
+    colors: {
+      white200: '#EAF6F6'
+    }
+  },
+  colors: {
+    background: {
+      gray700: '#161a1d',
+    }
+  }
+}
